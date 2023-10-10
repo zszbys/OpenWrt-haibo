05beffa393ef79c677be69864805ba2ce725ddea
jason <zsz417@163.com>
2023-10-11 07:01:19 +0800

    Update diy-zsz.sh

--- diy-zsz.sh
+++ diy-zsz.sh
@@ -36,7 +36,7 @@ svn export https://github.com/syb999/openwrt-19.07.1/trunk/package/network/servi
 git clone --depth=1 https://github.com/jerrykuku/luci-app-vssr package/luci-app-vssr
 git clone --depth=1 https://github.com/jerrykuku/lua-maxminddb package/lua-maxminddb
 # git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
-# svn export https://github.com/xiaorouji/openwrt-passwall/trunk/luci-app-passwall package/luci-app-passwall
+svn export https://github.com/xiaorouji/openwrt-passwall/trunk/luci-app-passwall package/luci-app-passwall
 # svn export https://github.com/xiaorouji/openwrt-passwall2/trunk/luci-app-passwall2 package/luci-app-passwall2
 svn export https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
 
